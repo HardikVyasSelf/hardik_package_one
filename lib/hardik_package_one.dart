9ea488b0ac20b3518c1dd44e8c4ba8685180bbc3
HardikVyasSelf <hardikvyas942@gmail.com>
2024-06-10 22:41:43 +0530

    - version changes

--- lib/hardik_package_one.dart
+++ lib/hardik_package_one.dart
@@ -1,7 +1,34 @@
-library hardik_package_one;
+// library hardik_package_one;
+//
+// import 'package:flutter/cupertino.dart';
+// import 'package:hardik_package_one/src/fancy_container.dart';
+// import 'package:hardik_package_two/hardik_package_two.dart';
+//
+// /// A Calculator.
+// class Calculator {
+//   /// Returns [value] plus 1.
+//   int addOne(int value) => value + 1;
+// }
+//
+// class TEST extends StatelessWidget {
+//   const TEST({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return const FancyContainer(
+//       size: 12,
+//     );
+//   }
+// }
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/cupertino.dart';
+import 'package:hardik_package_two/hardik_package_two.dart';
+
+class Cal extends StatelessWidget {
+  const Cal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("${Calculator().addOne(5)}");
+  }
 }
